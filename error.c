959c15f4e892ca5084bca136313a6dd50424e4f0
Daglar <daglareren.teksen@gmail.com>
2023-03-23 18:28:25 +0300

    some helpers added, equal sign>1 and par. errors handled

--- error.c
+++ error.c
@@ -1,6 +1,18 @@
 #include "token.h"
+#include "helpers.h"
 
 int isError (Token tokens[], int sizeOfTokens)
 {
-    return 1;
+    // if there are more than one equal sign in a line
+    if (tokenTypeCounter(EQUAL_SIGN, tokens, sizeOfTokens) > 1) {
+        return 1;
+    }
+
+    // if there is an error in parentheses
+    if (parCheck(tokens, sizeOfTokens)) {
+        return 1;
+    }
+
+    //
+    return 0;
 }
--- helpers.c
+++ helpers.c
@@ -1,5 +1,36 @@
+#include "token.h"
 void sliceString(char *string, char *destination, int start, int end) {
     for (int i = 0; i < end - start; ++i) {
         destination[i] = string[start + i];
     }
+}
+
+// counts how many same type tokens are
+int tokenTypeCounter(TokenType type, Token tokens[], int size) {
+    int count = 0;
+    for(int i=0 ; i<size ; ++i) {
+        if(tokens[i].tokenType = type) {
+            count++;
+        }
+    }
+    return count;
+}
+
+// counts how many parentheses are (output: 1 is error / 0 is no error)
+int parCheck(Token tokens[], int size) {
+    int count = 0;
+    for(int i=0 ; i<size ; ++i) {
+        if(tokens[i].tokenType = PARANTHESIS_OPENING) {
+            count++;
+        } else if(tokens[i].tokenType = PARANTHESIS_CLOSING) {
+            count--;
+        }
+        if (count<0) {
+            return 1;
+        }
+    }
+    if (count=!0) {
+        return 1;
+    }
+    return 0;
 }
