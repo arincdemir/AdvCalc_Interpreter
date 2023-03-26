3d2f2085944a01fb9891da7351f15a426e6fb18d
arincdemir <68223368+arincdemir@users.noreply.github.com>
2023-03-26 15:45:03 +0300

    added test code for infix postfix transformation

--- evaluator.c
+++ evaluator.c
@@ -82,6 +82,12 @@ int evaluate(Token tokens[], int begin, int end) {
     for (int i = begin; i < end + 1; ++i) {
         push(tokenList, tokens[i]);
     }
+    TokenList *postFix = infixToPostfix(tokenList);
+    int size = postFix->size;
+    for (int i = 0; i < size; ++i) {
+        Token pop = popBottom(postFix);
+        printf("token: %d , %d \n", pop.tokenType, pop.value);
+    }
     return 31;
 }
 
